63df3e33aa99a79de375d5a4a05bd34b26840f8f
Arlequinnn <1337538966@qq.com>
2020-03-16 13:25:02 +0800

    tutor-initial

---
+++ src/main/java/com/example/tutorselector/TutorSelectorApplication.java
@@ -0,0 +1,13 @@
+package com.example.tutorselector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorSelectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorSelectorApplication.class, args);
+    }
+
+}
